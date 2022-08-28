b405bd182d5c3c5a2aa4c7be1f5f76a3fa214179
Lilian <lwon8788@uni.sydney.edu.au>
2022-08-28 14:57:26 +1000

    cleaned up code a little

--- androidApp/src/main/java/com/ontask/android/MainActivity.kt
+++ androidApp/src/main/java/com/ontask/android/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.ontask.android
 
 import android.app.DatePickerDialog
-import android.content.Context
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -22,8 +23,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -40,10 +39,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
-import com.example.compose.ui.theme.ComposeTheme
-import com.google.android.material.datepicker.MaterialDatePicker
 import java.util.*
 
 class Register : ComponentActivity() {
@@ -66,45 +61,6 @@ class Register : ComponentActivity() {
 }
 
 @Composable
-fun DatePickerDemo(context: Context) {
-    val mYear: Int
-    val mMonth: Int
-    val mDay: Int
-    val now = Calendar.getInstance()
-    mYear = now.get(Calendar.YEAR)
-    mMonth = now.get(Calendar.MONTH)
-    mDay = now.get(Calendar.DAY_OF_MONTH)
-    now.time = Date()
-    val date = remember { mutableStateOf("") }
-    val datePickerDialog = DatePickerDialog(
-        context,
-        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
-            val cal = Calendar.getInstance()
-            cal.set(year, month, dayOfMonth)
-//            date.value = getFormattedDate(cal.time, "dd MMM,yyy")
-        }, mYear, mMonth, mDay
-    )
-
-    val day1= Calendar.getInstance()
-    day1.set(Calendar.DAY_OF_MONTH, 1)
-    datePickerDialog.datePicker.minDate = day1.timeInMillis
-    datePickerDialog.datePicker.maxDate = Calendar.getInstance().timeInMillis
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            datePickerDialog.show()
-        }) {
-            Text(text = "Click To Open Date Picker")
-        }
-        Spacer(modifier = Modifier.size(1.dp))
-        Text(text = "Selected date: ${date.value}")
-    }
-}
-
-@Composable
 fun Greeting(name: String) {
     var paddingState by remember { mutableStateOf(16.dp) }
     val padding by animateDpAsState(
@@ -123,11 +79,6 @@ fun Greeting(name: String) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
-//            Image(
-//                painter = ColorPainter(Color.Gray), contentDescription = "Gray", modifier = Modifier
-//                    .fillMaxHeight(0.2f)
-//                    .fillMaxWidth()
-//            )
 
             Text(
                 text = "Let's get started",
@@ -157,10 +108,7 @@ fun Greeting(name: String) {
                     usernameInput(this, localFocusManager)
                     emailInput(this, localFocusManager)
                     passwordInput(this, localFocusManager)
-//                    datePicker()
                     dateOfBirth(this, localFocusManager)
-//                    DatePickerDemo(context)
-
                 }
 
             Row(){
@@ -348,9 +296,9 @@ fun passwordInput(modifier: Modifier, localFocusManager: FocusManager): String{
     return password
 }
 
-
+// reference: https://www.geeksforgeeks.org/date-picker-in-android-using-jetpack-compose/
 @Composable
-fun dateOfBirth(modifier: Modifier = Modifier, localFocusManager: FocusManager): String {
+fun dateOfBirth(modifier: Modifier, localFocusManager: FocusManager): String {
     // Fetching the Local Context
     val mContext = LocalContext.current
 
@@ -389,7 +337,7 @@ fun dateOfBirth(modifier: Modifier = Modifier, localFocusManager: FocusManager):
         },
         modifier = Modifier
             .padding(12.dp)
-            .fillMaxWidth(0.75f)
+            .fillMaxWidth(0.755f)
             .height(56.dp),
         colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
         border = BorderStroke(1.dp, Color(0xff989898))
@@ -417,144 +365,8 @@ fun dateOfBirth(modifier: Modifier = Modifier, localFocusManager: FocusManager):
         }
     }
 
-    return ""
-
+    return mDate.value
 }
-
-
-// reference: https://www.geeksforgeeks.org/date-picker-in-android-using-jetpack-compose/
-@Composable
-fun datePicker() { // TODO: make this return the date of birth so that date of birth field can be filled?
-    // Fetching the Local Context
-    val mContext = LocalContext.current
-
-    // Declaring integer values
-    // for year, month and day
-    val mYear: Int
-    val mMonth: Int
-    val mDay: Int
-
-    // Initializing a Calendar
-    val mCalendar = Calendar.getInstance()
-
-    // Fetching current year, month and day
-    mYear = mCalendar.get(Calendar.YEAR)
-    mMonth = mCalendar.get(Calendar.MONTH)
-    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
-
-    mCalendar.time = Date()
-
-    // Declaring a string value to
-    // store date in string format
-    val mDate = remember { mutableStateOf("") }
-
-    // Declaring DatePickerDialog and setting
-    // initial values as current values (present year, month and day)
-    val mDatePickerDialog = DatePickerDialog(
-        mContext,
-        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
-            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
-        }, mYear, mMonth, mDay
-    )
-
-    Column(modifier = Modifier.padding(1.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-
-        // Creating a button that on
-        // click displays/shows the DatePickerDialog
-        Button(onClick = {
-            mDatePickerDialog.show()
-        }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0XFF0F9D58)) ) {
-            Text(
-                text = "Open Date Picker",
-                color = Color.White
-            )
-        }
-
-        // Adding a space of 100dp height
-        Spacer(modifier = Modifier.size(20.dp))
-
-        // Displaying the mDate value in the Text
-        Text(
-            text = "Date of birth: ${mDate.value}",
-            fontSize = 18.sp,
-            textAlign = TextAlign.Center
-        )
-    }
-
-}
-
-
-/*
-// reference: https://sgkantamani.medium.com/how-to-show-date-picker-in-jetpack-compose-8bc77a3ce408
-@Composable
-fun datePickerView(
-    datePicked : String?,
-    updatedDate : ( date : Long? ) -> Unit,
-) {
-    val activity = LocalContext.current as AppCompatActivity
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.TopStart)
-            .padding(top = 10.dp)
-            .border(0.5.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.5f))
-            .clickable{
-                showDatePicker(activity, updatedDate)
-            }
-    ) {
-
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-
-            val (lable, iconView) = createRefs()
-
-            Text(
-                text= datePicked?:"Date Picker",
-                color = MaterialTheme.colors.onSurface,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .constrainAs(lable) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(iconView.start)
-                        width = Dimension.fillToConstraints
-                    }
-            )
-
-            Icon(
-                imageVector = Icons.Default.DateRange,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(20.dp, 20.dp)
-                    .constrainAs(iconView) {
-                        end.linkTo(parent.end)
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                    },
-                tint = MaterialTheme.colors.onSurface
-            )
-
-        }
-
-    }
-}
-
-private fun showDatePicker(
-    activity : AppCompatActivity,
-    updatedDate: (Long?) -> Unit)
-{
-    val picker = MaterialDatePicker.Builder.datePicker().build()
-    picker.show(activity.supportFragmentManager, picker.toString())
-    picker.addOnPositiveButtonClickListener {
-        updatedDate(it)
-    }
-}
-*/
 
 @Preview(showBackground = true)
 @Composable
